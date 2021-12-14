af76a48645ec2069470d236e953a8c53cf9bcd23
Brian Gish <gish.brian@gmail.com>
2021-12-13 19:52:37 -0800

    Moving EditorUtil so it can be accessed at runtime (just returns null if not editor).

---
+++ Runtime/Utilities/EditorUtil.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EditorUtil.cs" company="Lost Signal LLC">
+//     Copyright (c) Lost Signal LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#if UNITY
+
+namespace Lost
+{
+    using UnityEngine;
+
+    public static class EditorUtil
+    {
+        public static T GetAssetByGuid<T>(string guid)
+            where T : UnityEngine.Object
+        {
+            if (Application.isEditor == false)
+            {
+                Debug.LogError("Trying to call EditorUtil.GetAssetByGuid from a build!");
+            }
+            else
+            {
+                #if UNITY_EDITOR
+                if (string.IsNullOrEmpty(guid))
+                {
+                    return null;
+                }
+
+                string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+
+                if (string.IsNullOrEmpty(path))
+                {
+                    return null;
+                }
+
+                return UnityEditor.AssetDatabase.LoadAssetAtPath<T>(path);
+                #endif
+            }
+
+            return null;
+        }
+    }
+}
+
+#endif
